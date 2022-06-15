f4636d738d1b0af9b0f229ace566cad0ee766435
Manisha.9801@gmail.com <manisha.9801@gmail.com>
2022-06-15 14:21:42 +0530

    Expenses feature implementation

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,16 @@
+import {useState} from 'react'
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "./../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+ const [filteredYear , setFilteredYear] = useState('2020')
+
+   const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear)
+  }
+  
   const expenseItems = props.expenses.map((expense, index) => {
     return (
       <ExpenseItem
@@ -13,7 +21,15 @@ function Expenses(props) {
       />
     );
   });
-  return  <Card className="expenses">{expenseItems}</Card>;
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter filteredYear={filteredYear} filterChangeHandler={filterChangeHandler} />
+        {expenseItems}
+      </Card>
+      ;
+    </div>
+  );
 }
 
 export default Expenses;
